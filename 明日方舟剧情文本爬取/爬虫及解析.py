333c29b6f450ab75f97e9941446ab292d2422505
dalin-hub <85146791+dalin-hub@users.noreply.github.com>
2021-08-25 13:16:11 +0800

    Update 爬虫及解析.py

--- 明日方舟剧情文本爬取/爬虫及解析.py
+++ 明日方舟剧情文本爬取/爬虫及解析.py
@@ -3,6 +3,15 @@
 Created on Mon Aug 23 23:13:06 2021
 
 @author: 24587
+此脚本针对prts.wiki进行了明日方舟游戏剧情的爬取，使用的方法是用正则来匹配网页中的对话内容，需要借助经过预处理产生的section_list列表，
+section_list：
+    type:list
+    elements:sect object
+sect object:每一个sect代表一章的索引
+    sect.text:从网页中的章节列表中爬到的内容
+    sect.name:章名
+    sect.title:章内的节名
+    sect.herf:节名对应的url参数
 """
 
 import requests
